d71aa6cc72cd91fd0904fa7526ea7dece87bae4b
Noe <noe@gmail.com>
2022-04-23 17:19:41 -0500

    first commit

---
+++ app/src/main/java/com/sierra/poketinder/SplashPokeTinder.kt
@@ -0,0 +1,20 @@
+package com.sierra.poketinder
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.AlarmClock
+
+class SplashPokeTinder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_poke_tinder)
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, "message")
+        }
+        Handler(Looper.getMainLooper()).postDelayed({ startActivity(intent); finish() },3000)
+    }
+}
